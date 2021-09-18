0f1627740117cab429fa05a9cdeee394b8b34b7b
david042 <david.mal04@hotmail.com>
2021-09-18 10:26:52 +0900

    adicionado um obstaculo

--- www/components/script.js
+++ www/components/script.js
@@ -17,9 +17,12 @@ window.onload = function(){
 
 var personagemObj;
 
+var obstaculo;
+
 function inicioJogo(){
   areaJogo.start();
   personagemObj = new componente("#FFF", 10, 120, 30, 30);
+  obstaculo = new componente("green", 150, 80, 10, 120);
 }
 
 let areaJogo = {
@@ -34,33 +37,65 @@ let areaJogo = {
   
   limpar: function(){
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+
+  parar: function(){
+    clearInterval(this.interval);
   }
 }
 
-function componente(cor, x, y, largura, altura){
-  this.x = x,
-  this.y = y,
+function componente(cor, x, y, altura, largura){
   this.altura = altura,
   this.largura = largura,
+  this.x = x,
+  this.y = y,
   this.velocidadeX = 0,
   this.velocidadeY = 0,
 
   this.atualiza = function(){
     contexto = areaJogo.context;
-    contexto.fillStyle = cor,
-    contexto.fillRect(this.x, this.y, this.largura, this.altura);
-  }
+    contexto.fillStyle = cor;
+    contexto.fillRect(this.x, this.y, this.altura, this.largura);
+  },
 
   this.novaPosicao = function(){
     this.x += this.velocidadeX;
     this.y += this.velocidadeY;
+  },
+
+  this.bater = function(obj){
+    //posição do personagem
+    let esquerda = this.x;
+    let direita = this.x + this.altura;
+    let superior = this.y;
+    let inferior = this.y + this.largura;
+    //posição do obstáculo
+    let objEsquerda = obj.x;
+    let objDireita = obj.x + obj.altura;
+    let objSuperior = obj.y;
+    let objInferior = obj.y + obj.largura;
+
+    let batida = true;
+
+    if(
+      (inferior < objSuperior) || (superior > objInferior) || 
+      (direita < objEsquerda) || (esquerda > objDireita)
+    ){
+      batida = false;
+    }
+    return batida;
   }
 }
 
 function atualizaAreaJogo(){
-  areaJogo.limpar();
-  personagemObj.novaPosicao();
-  personagemObj.atualiza();
+  if(personagemObj.bater(obstaculo)){
+    areaJogo.parar();
+  }else{
+    areaJogo.limpar();
+    obstaculo.atualiza();
+    personagemObj.novaPosicao();
+    personagemObj.atualiza();
+  }
 }
 
 function subir(){
